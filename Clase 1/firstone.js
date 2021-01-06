607480b09c3ed716b862dc584bde88a7072afe1f
Lucas Diaz Miguez <lucadiazmiguez@gmail.com>
2021-01-06 18:30:18 -0300

    ordené carpetas

---
+++ Clase 1/firstone.js
@@ -0,0 +1,35 @@
+// var x = prompt("ingrese su nombre");
+// var y = prompt("ingrese su apellido");
+
+// console.log("su nombre es %s %s",x,y);
+
+alert("Calculadora");
+
+x = prompt("ingrese el digito 1");
+y = prompt("ingrese el digito 2");
+a = Number("x");
+b = Number("y");
+var resultado;
+resultado =Number( a + b) ;
+alert(resultado);
+
+// console.log(typeof x +  typeof y +  typeof a +  + typeof b + );
+// ope = prompt("ingrese la operación que desea realizar ( +,-,*,%)");
+// switch (ope) {
+//     case '-':
+//         resultado = a * b;
+//         break;
+//     case '/':
+//         resultado = a / b;
+//         break;
+//     case '+':
+//         resultado = a + b;
+//         break;
+//     case '-':
+//         resultado = a - b;
+//         break;
+//     default:
+//         alert("metiste otra opcion");
+//         }
+// alert(resultado);
+// console.log(resultado);
