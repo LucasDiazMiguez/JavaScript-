702a8c63532409abcb9b4e6c40134dd45380bce0
Lucas Diaz Miguez <lucadiazmiguez@gmail.com>
2021-02-07 23:44:25 -0300

    Working on the shoppingCart

--- Ecommerce/js/main.js
+++ Ecommerce/js/main.js
@@ -1,6 +1,17 @@
-
 import dataBase from './classes.js';
-
+function AddtoCart(product)
+{
+    // for (let i = 0; i < shoppingCart.length; i++) {
+    //     if (producto== shoppingCart[i]) {
+            
+    //     }
+    // }
+    if (producto.stock>0) {
+        shoppingCart.push(product);
+        console.log(shoppingCart);
+        localStorage.setItem("shoppingCart",JSON.stringify(shoppingCart));
+    }
+}
 function cardCarusel(id, items) {
     let card = document.getElementById(id);
     console.log(items);
@@ -18,52 +29,58 @@ function cardCarusel(id, items) {
             </div>
             <div class="price-shop">
             <h6 class="galeria-precio"> ${items[i].price} </h6>
-            <button value="">agregar al carrito</button>
+            <button value="" onclick='AddtoCart(${JSON.stringify(items[i])})'>agregar al carrito</button>
             </div>
             </a>
             </div>`
-            
+         ;   
         }
     }
 }
 let cardItems = dataBase;
-// console.log(cardItems);
-
-// // let carrito=[];
-
-// if (localStorage.getItem("carrito"!=null)) {
-//     carrito=JSON.parse(localStorage.getItem("carrito"));
-// }
-
-// //loading the data into the "carusel"
-
-// let card='';
-// for (let i = 0; i < cardItems.length; i++) {
-//     // console.log(dataBase[i]);
-//     console.log(cardItems.length);   
-//     let card=document.getElementsByClassName("cardCarusel");
-//     console.log(card);
-//     if (cardItems[i].stock>0) {
-
-//         card[0].innerHTML= card+`
-//         <div class="col-lg-3 col-md-6 galeria-caja-de-item">
-//         <a href="">
-//         <div class="galeria-foto-titulo">
-//         <img src="${cardItems[i].image}"
-//         alt="${cardItems[i].description}">
-//         <h6 class="flotante">${cardItems[i].name}</h6>
-//         </div>
-//         <div class="price-shop">
-//         <h6 class="galeria-precio"> ${cardItems[i].price} </h6>
-//         <button value="">agregar al carrito</button>
-//         </div>
-//         </a>
-//         </div>`
-
-//     }
-
-// }
-
 cardCarusel('filaUno',cardItems);
 cardCarusel('filaDos',cardItems);
-//ahora hay que hacerlo para el otro
+// function showCarrito(shoppingCart){
+//     let divDeCompra= document.getElementById("contenedor-de-filas-carrito");
+//     for (let i = 0; i < shoppingCart.length; i++) {
+//         const element = shoppingCart[i];
+//         divDeCompra.innerHTML=+divDeCompra.innerHTML+`
+//         <div class="row justify-content-around align-items-center fila__producto ">        
+//         <div class="col-lg-3 fila__producto__eliminar__ignorar">
+//           <img src="${shoppingCart[i].image}" alt=" AMD Radeon 6800XT">
+//             <div class="checkboxes">
+//                 <div class="checkbox-sub">
+//                     <label for="eliminar">Eliminar</label>
+//                     <input type="checkbox" name="eliminar">
+//                 </div>
+//                 <div class="checkbox-sub">
+//                     <label for="ignorar">Ignorar</label>
+//                     <input type="checkbox" name="ignorar">
+//                 </div>
+//             </div>
+//         </div>
+//         <div class="col-lg-5">
+//             <h5> ${shoppingCart[i].name} </h5>
+//         </div>
+//         <div class="col-lg-3 fila__producto__numero__precio">
+//             <input type="number" name="cantidad" id="cantidad--de--productos">
+//             <!-- <div class="button-minus-plus">
+//                 <button>
+//                     <i class="fas fa-minus"></i>
+//                 </button>
+//                 <button>
+//                     <i class="fas fa-plus"></i>
+//                 </button>
+//             </div> -->
+//             <strong class="price"> Precio: ${shoppingCart[i].price}$ </strong>
+//         </div>
+
+//     </div>`
+//     }
+// }
+
+let shoppingCart = [];
+if (localStorage.getItem("shoppingCart")!=null) {
+    shoppingCart=JSON.parse(localStorage.getItem("shoppingCart"));
+} 
+// showCarrito(shoppingCart);
