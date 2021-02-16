bacf804cf5f395e22fbd4f968f2e0ec04f100386
Lucas Diaz Miguez <lucadiazmiguez@gmail.com>
2021-02-15 21:20:03 -0300

    Added a little bit of Jquery and made a new cart-icon

--- Ecommerce/js/main.js
+++ Ecommerce/js/main.js
@@ -1,7 +1,7 @@
 
 // import dataBase from './classes.js';
 class Products {
-    constructor(price, stock, image, description, name, id, marca,cantidad) {
+    constructor(price, stock, image, description, name, id, marca, cantidad) {
         this.price = price;
         this.stock = stock;
         this.image = image;
@@ -9,29 +9,29 @@ class Products {
         this.id = id;
         this.marca = marca;
         this.description = description;
-        this.cantidad=cantidad;
+        this.cantidad = cantidad;
     }
 }
 
 // console.log("ola");
-let producto1= new Products(4999,30,"imagenes/imagenesInicio/camara-destacada.webp","Camara Web Webcam Usb Pc Full Hd 1080p Plug & Play Microfono","Camara Web Webcam Usb Pc Full Hd 1080p Plug & Play Microfono","000000001","none");
+let producto1 = new Products(4999, 30, "imagenes/imagenesInicio/camara-destacada.webp", "Camara Web Webcam Usb Pc Full Hd 1080p Plug & Play Microfono", "Camara Web Webcam Usb Pc Full Hd 1080p Plug & Play Microfono", "000000001", "none");
 
-let producto2= new Products(15999,20,"imagenes/imagenesInicio/gabinetegamer.webp","Gabinete Sentey Z20 Lite - Led Rgb","Gabinete Sentey Z20 Lite - Led Rgb","000000002","sentey");
+let producto2 = new Products(15999, 20, "imagenes/imagenesInicio/gabinetegamer.webp", "Gabinete Sentey Z20 Lite - Led Rgb", "Gabinete Sentey Z20 Lite - Led Rgb", "000000002", "sentey");
 
-let producto3= new Products(150999,10,"imagenes/imagenesInicio/GPU.webp","Placa Video Msi Geforce Rtx2070 Super Ventus Gp Oc 8gb Gddr","Placa Video Msi Geforce Rtx2070 Super Ventus Gp Oc 8gb Gddr","000000003","nvidia");
- 
-let producto4= new Products(26999,16,"imagenes/imagenesInicio/motherboardbarata-destacado.webp","Motherboard Gigabyte Ga-b365m Elite Intel 1151 9na 4","Motherboard Gigabyte Ga-b365m Elite Intel 1151 9na 4","000000004","gigabyte");
+let producto3 = new Products(150999, 10, "imagenes/imagenesInicio/GPU.webp", "Placa Video Msi Geforce Rtx2070 Super Ventus Gp Oc 8gb Gddr", "Placa Video Msi Geforce Rtx2070 Super Ventus Gp Oc 8gb Gddr", "000000003", "nvidia");
 
-let producto5= new Products(3999,16,"imagenes/imagenesInicio/reloj-destacado.webp","Xiaomi Mi Band 5 Global Smart Watch Reloj Inteligente + Film","Xiaomi Mi Band 5 Global Smart Watch Reloj Inteligente + Film","000000005","xiaomi");
+let producto4 = new Products(26999, 16, "imagenes/imagenesInicio/motherboardbarata-destacado.webp", "Motherboard Gigabyte Ga-b365m Elite Intel 1151 9na 4", "Motherboard Gigabyte Ga-b365m Elite Intel 1151 9na 4", "000000004", "gigabyte");
 
-let producto6= new Products(180999,26,"imagenes/imagenesInicio/rtx 2080ti.webp","Placa Video Geforce Strix Gaming 2080ti Ddr6 11gb Rog ","Placa Video Geforce Strix Gaming 2080ti Ddr6 11gb Rog ","000000006","nvidia");
+let producto5 = new Products(3999, 16, "imagenes/imagenesInicio/reloj-destacado.webp", "Xiaomi Mi Band 5 Global Smart Watch Reloj Inteligente + Film", "Xiaomi Mi Band 5 Global Smart Watch Reloj Inteligente + Film", "000000005", "xiaomi");
 
-let producto7= new Products(5999,26,"imagenes/imagenesInicio/ssd.webp","Disco sólido interno Kingston SA400S37/480G 480GB","Disco sólido interno Kingston SA400S37/480G 480GB","000000007","kingston");
+let producto6 = new Products(180999, 26, "imagenes/imagenesInicio/rtx 2080ti.webp", "Placa Video Geforce Strix Gaming 2080ti Ddr6 11gb Rog ", "Placa Video Geforce Strix Gaming 2080ti Ddr6 11gb Rog ", "000000006", "nvidia");
 
-let producto8= new Products(85099,26,"imagenes/imagenesInicio/indice.webp","Notebook Hp 14-cf3047la I3-1005g1 4gb 256gb Ssd Windows 10","Notebook Hp 14-cf3047la I3-1005g1 4gb 256gb Ssd Windows 10","000000008","hp");
+let producto7 = new Products(5999, 26, "imagenes/imagenesInicio/ssd.webp", "Disco sólido interno Kingston SA400S37/480G 480GB", "Disco sólido interno Kingston SA400S37/480G 480GB", "000000007", "kingston");
 
-let dataBaseMasVendidos=[producto1,producto2,producto3,producto4];
-let dataBaseDestacados=[producto5,producto6,producto7,producto8];
+let producto8 = new Products(85099, 26, "imagenes/imagenesInicio/indice.webp", "Notebook Hp 14-cf3047la I3-1005g1 4gb 256gb Ssd Windows 10", "Notebook Hp 14-cf3047la I3-1005g1 4gb 256gb Ssd Windows 10", "000000008", "hp");
+
+let dataBaseMasVendidos = [producto1, producto2, producto3, producto4];
+let dataBaseDestacados = [producto5, producto6, producto7, producto8];
 
 
 function cardCarusel(id, items) {
@@ -67,16 +67,16 @@ if ((estoyenindex) != null) {
     cardCarusel('filaDos', dataBaseDestacados);
 }
 
-function addAnotherOne(producto){
-    if(producto.cantidad <= producto.stock)
-    producto.cantidad=producto.cantidad+1;
+function addAnotherOne(producto) {
+    if (producto.cantidad <= producto.stock)
+        producto.cantidad = producto.cantidad + 1;
     return producto;
 }
 
 function AddtoCart(product) {
     if (product.stock > 0) {
         console.log("im in");
-        product.stock=product.stock-1;
+        product.stock = product.stock - 1;
 
         console.log("yo soy shopping cart" + shoppingCart)
         shoppingCart.push(product);
@@ -85,11 +85,11 @@ function AddtoCart(product) {
         //como la página se carga de nuevo se muestra solito el shoppingcart por el llamado que está abajo del todo
     }
 }
-function deleteProduct(product){
-    let newShoppingCart=[];
-    let variabledeayuda=0;
-    shoppingCart=JSON.parse(localStorage.getItem("shoppingCart"));
-    
+function deleteProduct(product) {
+    let newShoppingCart = [];
+    let variabledeayuda = 0;
+    shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
+
     for (let i = 0; i < shoppingCart.length; i++) {
         console.log("yo soy producto")
         console.log(product);
@@ -97,28 +97,31 @@ function deleteProduct(product){
         console.log(shoppingCart[i]);
         if (product.id == shoppingCart[i].id) {
             console.log("no hago nada")
-            variabledeayuda=1;
+            variabledeayuda = 1;
         } else {
             console.log("toy aka");
-            newShoppingCart[i-variabledeayuda]=shoppingCart[i];
-        }        
-    }    
-    console.log( "dsps de eliminar" );
+            newShoppingCart[i - variabledeayuda] = shoppingCart[i];
+        }
+    }
+    console.log("dsps de eliminar");
     console.log(newShoppingCart);
     localStorage.setItem("shoppingCart", JSON.stringify(newShoppingCart));
     showCarrito(JSON.parse(localStorage.getItem("shoppingCart")));
-}   
+}
 
 
 function showCarrito(shoppingCart) {
+    let aux=shoppingCart.length; 
+    $("#cart-icon-b").text("Carrito "+aux);
     if (localStorage.getItem("shoppingCart") != null) {
         shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
         console.log(shoppingCart);
         let divDeCompra = document.getElementById("contenedor-de-filas-carrito");
-        let preciototal=0; 
-        let aux=0;
+        let preciototal = 0;
+        let aux = 0;
+        
         for (let i = 0; i < shoppingCart.length; i++) {
-            preciototal= preciototal +shoppingCart[i].price;
+            preciototal = preciototal + shoppingCart[i].price;
             aux = aux + `
             <div class="row justify-content-around align-items-center fila__producto ">        
             <div class="col-lg-3 fila__producto__eliminar__ignorar">
@@ -153,7 +156,7 @@ function showCarrito(shoppingCart) {
         }
         console.log(aux);
         divDeCompra.innerHTML = aux +
-         `<div class="row justify-content-around align-items-center fila__confirmar__pago">
+            `<div class="row justify-content-around align-items-center fila__confirmar__pago">
         <div class="col-lg-5">
         </div>
         <div class="col-lg-3">
@@ -166,10 +169,10 @@ function showCarrito(shoppingCart) {
         </div>`
         console.log("entre al if del showcarrito")
     }
-     if(shoppingCart.length==0){
-        console.log("entre al else del showcarrito")
+    if (shoppingCart.length == 0) {
+        // console.log("entre al else del showcarrito")
         let divDeCompra = document.getElementById("contenedor-de-filas-carrito");
-        divDeCompra.innerHTML=`<div class="row justify-content-around align-items-center fila__producto ">        
+        divDeCompra.innerHTML = `<div class="row justify-content-around align-items-center fila__producto ">        
         <div class="col-lg-8 fila__producto__eliminar__ignorar">
         </div>
         <div class="col-lg-7">
@@ -182,6 +185,7 @@ let shoppingCart = [];
 if (localStorage.getItem("shoppingCart") != null) {
     shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
 }
+
 showCarrito(shoppingCart);
 
 
