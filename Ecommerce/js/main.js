37ad0b6133f1be21e1e4dc3f212257c3c61a4a4c
Lucas Diaz Miguez <lucadiazmiguez@gmail.com>
2021-03-04 19:03:52 -0300

    now you can delete and add items without problem

--- Ecommerce/js/main.js
+++ Ecommerce/js/main.js
@@ -1,6 +1,6 @@
 
 class Products {
-    constructor(price, stock, image, description, name, id, marca, cantidad) {
+    constructor(price, stock, image, description, name, id, marca, cantidadAgregada) {
         this.price = price;
         this.stock = stock;
         this.image = image;
@@ -8,36 +8,36 @@ class Products {
         this.id = id;
         this.marca = marca;
         this.description = description;
-        this.cantidad = cantidad;
+        this.cantidadAgregada = cantidadAgregada;
     }
 }
 
-let producto1 = new Products(4999, 30, "imagenes/imagenesInicio/camara-destacada.webp", "Camara Web Webcam Usb Pc Full Hd 1080p Plug & Play Microfono", "Camara Web Webcam Usb Pc Full Hd 1080p Plug & Play Microfono", "000000001", "none");
+let producto1 = new Products(4999, 30, "imagenes/imagenesInicio/camara-destacada.webp", "Camara Web Webcam Usb Pc Full Hd 1080p Plug & Play Microfono", "Camara Web Webcam Usb Pc Full Hd 1080p Plug & Play Microfono", "000000001", "none", 0);
 
-let producto2 = new Products(15999, 20, "imagenes/imagenesInicio/gabinetegamer.webp", "Gabinete Sentey Z20 Lite - Led Rgb", "Gabinete Sentey Z20 Lite - Led Rgb", "000000002", "sentey");
+let producto2 = new Products(15999, 20, "imagenes/imagenesInicio/gabinetegamer.webp", "Gabinete Sentey Z20 Lite - Led Rgb", "Gabinete Sentey Z20 Lite - Led Rgb", "000000002", "sentey", 0);
 
-let producto3 = new Products(150999, 10, "imagenes/imagenesInicio/GPU.webp", "Placa Video Msi Geforce Rtx2070 Super Ventus Gp Oc 8gb Gddr", "Placa Video Msi Geforce Rtx2070 Super Ventus Gp Oc 8gb Gddr", "000000003", "nvidia");
+let producto3 = new Products(150999, 10, "imagenes/imagenesInicio/GPU.webp", "Placa Video Msi Geforce Rtx2070 Super Ventus Gp Oc 8gb Gddr", "Placa Video Msi Geforce Rtx2070 Super Ventus Gp Oc 8gb Gddr", "000000003", "nvidia", 0);
 
-let producto4 = new Products(26999, 16, "imagenes/imagenesInicio/motherboardbarata-destacado.webp", "Motherboard Gigabyte Ga-b365m Elite Intel 1151 9na 4", "Motherboard Gigabyte Ga-b365m Elite Intel 1151 9na 4", "000000004", "gigabyte");
+let producto4 = new Products(26999, 16, "imagenes/imagenesInicio/motherboardbarata-destacado.webp", "Motherboard Gigabyte Ga-b365m Elite Intel 1151 9na 4", "Motherboard Gigabyte Ga-b365m Elite Intel 1151 9na 4", "000000004", "gigabyte", 0);
 
-let producto5 = new Products(3999, 16, "imagenes/imagenesInicio/reloj-destacado.webp", "Xiaomi Mi Band 5 Global Smart Watch Reloj Inteligente + Film", "Xiaomi Mi Band 5 Global Smart Watch Reloj Inteligente + Film", "000000005", "xiaomi");
+let producto5 = new Products(3999, 16, "imagenes/imagenesInicio/reloj-destacado.webp", "Xiaomi Mi Band 5 Global Smart Watch Reloj Inteligente + Film", "Xiaomi Mi Band 5 Global Smart Watch Reloj Inteligente + Film", "000000005", "xiaomi", 0);
 
-let producto6 = new Products(180999, 26, "imagenes/imagenesInicio/rtx 2080ti.webp", "Placa Video Geforce Strix Gaming 2080ti Ddr6 11gb Rog ", "Placa Video Geforce Strix Gaming 2080ti Ddr6 11gb Rog ", "000000006", "nvidia");
+let producto6 = new Products(180999, 26, "imagenes/imagenesInicio/rtx 2080ti.webp", "Placa Video Geforce Strix Gaming 2080ti Ddr6 11gb Rog ", "Placa Video Geforce Strix Gaming 2080ti Ddr6 11gb Rog ", "000000006", "nvidia", 0);
 
-let producto7 = new Products(5999, 26, "imagenes/imagenesInicio/ssd.webp", "Disco sólido interno Kingston SA400S37/480G 480GB", "Disco sólido interno Kingston SA400S37/480G 480GB", "000000007", "kingston");
+let producto7 = new Products(5999, 26, "imagenes/imagenesInicio/ssd.webp", "Disco sólido interno Kingston SA400S37/480G 480GB", "Disco sólido interno Kingston SA400S37/480G 480GB", "000000007", "kingston", 0);
 
-let producto8 = new Products(85099, 26, "imagenes/imagenesInicio/indice.webp", "Notebook Hp 14-cf3047la I3-1005g1 4gb 256gb Ssd Windows 10", "Notebook Hp 14-cf3047la I3-1005g1 4gb 256gb Ssd Windows 10", "000000008", "hp");
+let producto8 = new Products(85099, 26, "imagenes/imagenesInicio/indice.webp", "Notebook Hp 14-cf3047la I3-1005g1 4gb 256gb Ssd Windows 10", "Notebook Hp 14-cf3047la I3-1005g1 4gb 256gb Ssd Windows 10", "000000008", "hp", 0);
 
 let dataBaseMasVendidos = [producto1, producto2, producto3, producto4];
- let dataBaseDestacados = [producto5, producto6, producto7, producto8];
- fetch('../json/productos.json')
- .then(function (response) {
-   return response.json();
- })
- .then(function (data) {
-   console.log("los datos se cargaron correctamente");
-   console.log(data); 
- })
+let dataBaseDestacados = [producto5, producto6, producto7, producto8];
+fetch('../json/productos.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log("los datos se cargaron correctamente");
+        console.log(data);
+    })
 
 function cardCarusel(id, items) {
     let card = document.getElementById(id);
@@ -47,59 +47,64 @@ function cardCarusel(id, items) {
         if (items[i].stock > 0) {
             card.innerHTML = card.innerHTML + `
             <div class="col-lg-3     col-md-6 box-gallery-item">
-            <div>
+            <div id="cardtick">
             <img src="${items[i].image}"
                 alt="${items[i].description}">
             <h6 class="item-name">${items[i].name}</h6>
             <h6 class="cardprice">$ ${items[i].price} </h6>
-            <button value=""   onclick='AddtoCart(${JSON.stringify(items[i])})'>agregar al carrito</button>
+            <button id="${items[i].id}"value=""   onclick='AddtoCart(${JSON.stringify(items[i])})'>agregar al carrito</button>
         </div>
         </div>
             `;
         }
     }
 }
-//estoyenindex es una variable flag, o sea si está en  el html carrito es NULL y no ejecuta las dos funciones, si esta en index si las ejecuta
-let estoyenindex = document.getElementById("gallery-products");
-console.log(estoyenindex);
-if ((estoyenindex) != null) {
-    cardCarusel('row-One', dataBaseMasVendidos);
-    cardCarusel('row-Two', dataBaseDestacados);
-}
-
-function addAnotherOne(producto) {
-    if (producto.cantidad <= producto.stock)
-        producto.cantidad = producto.cantidad + 1;
-    return producto;
-}
-
 function AddtoCart(product) {
     if (product.stock > 0) {
         console.log("im in");
         product.stock = product.stock - 1;
-
+        let flag = 0;
         console.log("yo soy shopping cart" + shoppingCart)
-        shoppingCart.push(product);
-        console.log(shoppingCart.length);
+        for (let i = 0; i < shoppingCart.length; i++) {
+            if (product.id == shoppingCart[i].id) {
+                shoppingCart[i].cantidadAgregada = shoppingCart[i].cantidadAgregada + 1;
+                flag = 1;
+            }
+        }
+        if (flag == 0) {
+            product.cantidadAgregada = product.cantidadAgregada + 1;
+            console.log("entro acá");
+            console.log(product.cantidadAgregada);
+            shoppingCart.push(product);
+        }
         localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
-        //como la página se carga de nuevo se muestra solito el shoppingcart por el llamado que está abajo del todo
+        let  inicio = document.getElementById("gallery-products");
+
+        if (inicio!=null) {
+            let bottonCheck = document.getElementById(product.id);//this is the same Id as the card object
+            bottonCheck.innerHTML = bottonCheck.innerHTML + `<i class="fas fa-check"></i>`;
+        }
+
     }
+    showCarrito(shoppingCart);
     cardlength();
 }
 function deleteProduct(product) {
     let newShoppingCart = [];
     let variabledeayuda = 0;
-    
+
     shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
 
     for (let i = 0; i < shoppingCart.length; i++) {
-        console.log("yo soy producto")
         console.log(product);
-        console.log("yo soy shoppingcart")
         console.log(shoppingCart[i]);
         if (product.id == shoppingCart[i].id) {
-            console.log("no hago nada")
-            variabledeayuda = 1;
+            if (product.cantidadAgregada>1) {
+                shoppingCart[i].cantidadAgregada=shoppingCart[i].cantidadAgregada-1;
+                newShoppingCart[i] = shoppingCart[i];
+            }else{
+                variabledeayuda = 1;
+            }
         } else {
             console.log("toy aka");
             newShoppingCart[i - variabledeayuda] = shoppingCart[i];
@@ -107,16 +112,15 @@ function deleteProduct(product) {
     }
     console.log("dsps de eliminar");
     console.log(newShoppingCart);
-    shoppingCart=newShoppingCart;
+    shoppingCart = newShoppingCart;
     localStorage.setItem("shoppingCart", JSON.stringify(newShoppingCart));
     showCarrito(JSON.parse(localStorage.getItem("shoppingCart")));
     cardlength();
 }
-function cardlength(){
-    let aux=shoppingCart.length; 
-    $("#cart-icon-b").text("Carrito "+aux);
+function cardlength() {
+    let aux = shoppingCart.length;
+    $("#cart-icon-b").text("Carrito " + aux);
 }
-
 function showCarrito(shoppingCart) {
     cardlength();
     if (localStorage.getItem("shoppingCart") != null) {
@@ -125,40 +129,42 @@ function showCarrito(shoppingCart) {
         let divDeCompra = document.getElementById("contenedor-de-filas-carrito");
         let preciototal = 0;
         let aux = 0;
-        
+
         for (let i = 0; i < shoppingCart.length; i++) {
-            preciototal = preciototal + shoppingCart[i].price;
+            preciototal = preciototal + (shoppingCart[i].cantidadAgregada) * shoppingCart[i].price;
             aux = aux + `
-            <div class="row justify-content-around align-items-center fila__producto ">        
+            <div class="row justify-content-around align-items-center fila__producto ">
             <div class="col-lg-3 fila__producto__eliminar__ignorar">
-            <img src="${shoppingCart[i].image}" alt=" AMD Radeon 6800XT">
+                <img src="${shoppingCart[i].image}" alt=" AMD Radeon 6800XT">
                 <div class="checkboxes">
                     <div class="checkbox-sub">
-                        <button name="eliminar" onclick='deleteProduct(${JSON.stringify(shoppingCart[i])})'>eliminar</button>
+                        <button name="eliminar"
+                            onclick='deleteProduct(${JSON.stringify(shoppingCart[i])})'>eliminar</button>
                     </div>
                     <div class="checkbox-sub">
                         <label for="ignorar">Ignorar</label>
                         <input type="checkbox" name="eliminar">
-
                     </div>
                 </div>
             </div>
             <div class="col-lg-5">
                 <h5> ${shoppingCart[i].name} </h5>
             </div>
-            <div class="col-lg-3 fila__producto__numero__precio">
-                <input type="number" name="cantidad" id="cantidad--de--productos">
-                <!-- <div class="button-minus-plus">
-                    <button>
+            <div class="col-lg-4 fila__producto__numero__precio">
+                <div>
+                    <button onclick='deleteProduct(${JSON.stringify(shoppingCart[i])})'>
                         <i class="fas fa-minus"></i>
                     </button>
-                    <button>
+                    <input type="number" value="${shoppingCart[i].cantidadAgregada}" name="cantidad"
+                        id="cantidad--de--productos">
+                    <button onclick='AddtoCart(${JSON.stringify(shoppingCart[i])})'>
                         <i class="fas fa-plus"></i>
                     </button>
-                </div> -->
-                <strong class="price"> Precio: ${shoppingCart[i].price}$ </strong>
+                </div>
+                <strong class="price"> Precio: ${shoppingCart[i].cantidadAgregada * shoppingCart[i].price}$ </strong>
             </div>
-        </div>`
+            </div>
+            `
         }
         console.log(aux);
         divDeCompra.innerHTML = aux +
@@ -173,7 +179,6 @@ function showCarrito(shoppingCart) {
         </div>
 
         </div>`
-        console.log("entre al if del showcarrito")
     }
     if (shoppingCart.length == 0) {
         // console.log("entre al else del showcarrito")
@@ -186,6 +191,14 @@ function showCarrito(shoppingCart) {
         </div>
     </div>`
     }
+}
+//index its a flag variable, if it isnt in index html, index=null,else  equals something different than null
+
+let index = document.getElementById("gallery-products");
+console.log(index);
+if ((index) != null) {
+    cardCarusel('row-One', dataBaseMasVendidos);
+    cardCarusel('row-Two', dataBaseDestacados);
 }
 let shoppingCart = [];
 if (localStorage.getItem("shoppingCart") != null) {
